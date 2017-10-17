6c2187eb2610f5e707c19dc5bb0f00361c260e45
Dominic J. Bednar <32824861+G0DJ@users.noreply.github.com>
2017-10-17 12:10:02 -0400

    added sort pipe

--- do-uniq-species.sh
+++ do-uniq-species.sh
@@ -3,12 +3,12 @@
 # Author: Dominic J. Bednar, EIT
 
 # Loop over all files 
-
 for data_file in $@ 
 	do 
 	echo "Unique species in $data_file"
 	# Extract species names from data_file
-	cut -d , -f 2 $data_file
+	cut -d , -f 2 $data_file | sort | uniq
+	done
 
 # test pushing 
 
